fbdd131210ebd861138f4c56f49f22d3e0eada09
paulaleguisamo <padalefa@gmail.com>
2020-10-10 16:25:43 -0300

    Entrega 05 - Carrito de compras

--- js/cart.js
+++ js/cart.js
@@ -1,6 +1,87 @@
+let totalesPorArticulos = [];
+let total_articles_count = [];
+const reducer = (accumulator, currentValue) => accumulator + currentValue;
+total = 0;
+
+function showCartProducts(array) {
+    let htmlCommentsToAppend = "";
+
+    for (let i = 0; i < array.length; i++) {
+        total_articles_count.push(articles[i].count)
+    }
+
+    for (var i = 0; i < array.length; i++) {
+        let articles = array[i];
+
+        if (articles.currency == "USD") {
+            totalesPorArticulos[i] = articles.unitCost * 40 * total_articles_count[i];
+        } else if (articles.currency == "UYU") {
+            totalesPorArticulos[i] = articles.unitCost * total_articles_count[i];
+        }
+
+        htmlCommentsToAppend += `
+        <div class="list-group-item list-group-item-action">
+            <div class="row">
+                <div class="col-3">
+                    <img src="` + articles.src + `" alt="" class="img-thumbnail">
+                </div>
+                <div class="col">
+                    <div class="d-flex w-100 justify-content-between">
+                        <h4 class="mb-1">`+ articles.name + `</h4>
+                        <small> Seleccione la cantidad:`+ " " + `<br> <input type="number" id="cantidad` + i + `"  min="1"></input></small>
+                    </div>
+                    <small class="text-muted"> Articulos a comprar: `+ total_articles_count[i] + ` </small>
+                    <p class="mb-1"> Precio unitario:`+ " " + articles.currency + " " + articles.unitCost + ` </p>
+                    <p class="mb-1"> Total por articulo: UYU`+ " " + totalesPorArticulos[i] + ` </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        `;
+    }
+    document.getElementById("commentscontainer").innerHTML = htmlCommentsToAppend;
+
+
+    for (let i = 0; i < array.length; i++) {
+        document.getElementById("cantidad" + i).addEventListener("change", (e) => {
+            total_articles_count[i] = document.getElementById("cantidad" + i).value;
+            showCartProducts(articles);
+            showPrices(articles);
+        });
+    }
+}
+
+
+function showPrices(array) {
+    let htmlCommentsToAppend2 = "";
+
+    for (let i = 0; i < array.length; i++) {
+        let articles = array[i];
+
+        htmlCommentsToAppend2 += `
+        <p>`+ articles.name + " : UYU " + totalesPorArticulos[i] + `</p>
+        `;
+    }
+
+    document.getElementById("subtotales").innerHTML = htmlCommentsToAppend2;
+
+    total = totalesPorArticulos.reduce(reducer);
+    document.getElementById("totaldelpedido").innerHTML = " UYU " + total;
+}
+
+
+
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
-
+document.addEventListener("DOMContentLoaded", function (e) {
+    getJSONData(CART_INFO_URL)
+        .then(function (resultObj) {
+            if (resultObj.status === "ok") {
+                articles = resultObj.data.articles;
+                showCartProducts(articles)
+                showPrices(articles);
+            }
+        });
 });
+
--- js/init.js
+++ js/init.js
@@ -4,7 +4,7 @@ const CATEGORY_INFO_URL = "https://japdevdep.github.io/ecommerce-api/category/12
 const PRODUCTS_URL = "https://japdevdep.github.io/ecommerce-api/product/all.json";
 const PRODUCT_INFO_URL = "https://japdevdep.github.io/ecommerce-api/product/5678.json";
 const PRODUCT_INFO_COMMENTS_URL = "https://japdevdep.github.io/ecommerce-api/product/5678-comments.json";
-const CART_INFO_URL = "https://japdevdep.github.io/ecommerce-api/cart/987.json";
+const CART_INFO_URL = "https://japdevdep.github.io/ecommerce-api/cart/654.json";
 const CART_BUY_URL = "https://japdevdep.github.io/ecommerce-api/cart/buy.json";
 
 var showSpinner = function () {
@@ -40,7 +40,7 @@ var getJSONData = function (url) {
     });
 }
 
-
+//Redirección de Login-Index
 if (
   !window.location.href.endsWith("login.html") &&
   !sessionStorage.getItem("login")) {
@@ -48,16 +48,51 @@ if (
 }
 
 
+//Menú desplegable con el nombre del Usuario
+var divDropDown = document.createElement("div");
+divDropDown.setAttribute("class", "dropdown");
+document.getElementById("menu").appendChild(divDropDown);
+
+var nombreDeUsuario = document.createElement("a");
+nombreDeUsuario.setAttribute("role", "button");
+nombreDeUsuario.setAttribute("data-toggle", "dropdown");
+nombreDeUsuario.setAttribute("id", "usuario");
+nombreDeUsuario.setAttribute("class", "py-2 d-none d-md-inline-block btn btn-secondary dropdown-toggle");
+nombreDeUsuario.setAttribute("href", "#");
+nombreDeUsuario.setAttribute("aria-haspopup", "true");
+nombreDeUsuario.setAttribute("aria-expanded", "false");
+divDropDown.appendChild(nombreDeUsuario);
+
+var dropDownMenu = document.createElement("div");
+dropDownMenu.setAttribute("class", "dropdown-menu");
+dropDownMenu.setAttribute("aria-labelledby", "dropdownMenuLink");
+divDropDown.appendChild(dropDownMenu);
+
+var perfilDelUsuario = document.createElement("a");
+perfilDelUsuario.setAttribute("class", "dropdown-item");
+perfilDelUsuario.setAttribute("href", "my-profile.html");
+perfilDelUsuario.innerHTML = "Mi Perfil";
+dropDownMenu.appendChild(perfilDelUsuario);
+
+var carritoDelUsuario = document.createElement("a");
+carritoDelUsuario.setAttribute("class", "dropdown-item");
+carritoDelUsuario.setAttribute("href", "cart.html");
+carritoDelUsuario.innerHTML = "Mi Carrito";
+dropDownMenu.appendChild(carritoDelUsuario);
+
+var cerrarSesion = document.createElement("a");
+cerrarSesion.setAttribute("class", "dropdown-item");
+cerrarSesion.setAttribute("id", "cerrar-sesion");
+cerrarSesion.setAttribute("href", "login.html");
+cerrarSesion.innerHTML = "Cerrar Sesión";
+dropDownMenu.appendChild(cerrarSesion);
+
+//Función de colocar Nombre de Usuario y Cierre Sesión
 document.addEventListener("DOMContentLoaded", function (e) {
   let userLogged = sessionStorage.getItem("login");
   document.getElementById("usuario").innerHTML = localStorage.getItem("cuenta");
 
+  document.getElementById("cerrar-sesion").addEventListener("click", (evento) => {
+    sessionStorage.removeItem("login");
+  });
 });
-
-
-var nombreDeUsuario = document.createElement("a");
-nombreDeUsuario.setAttribute("id", "usuario");
-nombreDeUsuario.setAttribute("class", "py-2 d-none d-md-inline-block");
-nombreDeUsuario.setAttribute("href", "my-profile.html");
-var element = document.getElementById("menu");
-element.appendChild(nombreDeUsuario);
